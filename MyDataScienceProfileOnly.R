36d80c896984735b40f596f76d374a46c8709fc1
BrianYuDS <brian.yu.09@gmail.com>
2016-10-06 16:39:59 -0500

    Update MyDataScienceProfileOnly.R

--- MyDataScienceProfileOnly.R
+++ MyDataScienceProfileOnly.R
@@ -20,7 +20,6 @@ ranking
 # Presentation Skills   3
 # Data Visualization    3
 
-
 # Create data frame
 brian <- data.frame(categories, ranking)
 
@@ -31,4 +30,19 @@ brian
 barplot(xtabs(brian$ranking~brian$categories), space = F, 
         col = rainbow(8), main = "Brian's Data Science Profile")
 
+# SessionInfo
+# > sessionInfo()
+# R version 3.3.1 (2016-06-21)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 7 x64 (build 7601) Service Pack 1
 
+# locale:
+# [1] LC_COLLATE=English_United States.1252  LC_CTYPE=English_United States.1252    LC_MONETARY=English_United States.1252
+# [4] LC_NUMERIC=C                           LC_TIME=English_United States.1252    
+
+# attached base packages:
+# [1] stats     graphics  grDevices utils     datasets  methods   base     
+
+# loaded via a namespace (and not attached):
+# [1] magrittr_1.5    htmltools_0.3.5 tools_3.3.1     yaml_2.1.13     Rcpp_0.12.7     stringi_1.1.1   rmarkdown_1.0  
+# [8] stringr_1.1.0   digest_0.6.10   evaluate_0.9   
